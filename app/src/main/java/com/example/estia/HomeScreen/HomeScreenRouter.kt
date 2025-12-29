670bf21a344470db4c0ac81d089aa5c71790b47b
Zia Cheema <cheemazia863@gmail.com>
2025-12-29 23:35:26 +0500

    up

---
+++ app/src/main/java/com/example/estia/HomeScreen/HomeScreenRouter.kt
@@ -0,0 +1,7 @@
+package com.example.estia.HomeScreen
+
+object HomeScreenRouter{
+    val mainScreen = "MainScreen"
+    val globalCharts = "globalChartsScreen"
+    val artistDisplayScreen = "artistDisplayScreen"
+}
